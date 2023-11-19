7bc48b0332d3ab58a11d6641ca1ccfad86de6978
Michael-Zapivahin <Zapivahin@ya.ru>
2023-11-19 17:35:06 +0300

    add docker

---
+++ data.py
@@ -0,0 +1,16 @@
+
+
+fields_template = [
+    {'id': 1, 'name': 'email', 'type': 'email', 'template_id': 'lk'},
+    {'id': 2, 'name': 'phone', 'type': 'phone_number', 'template_id': 'lk'},
+    {'id': 3, 'name': 'date', 'type': 'date', 'template_id': 'lk'},
+    {'id': 4, 'name': 'text', 'type': 'text', 'template_id': 'lk'},
+    {'id': 5, 'name': 'email', 'type': 'email', 'template_id': 'authorization'},
+    {'id': 6, 'name': 'login', 'type': 'text', 'template_id': 'authorization'},
+    {'id': 7, 'name': 'password', 'type': 'text', 'template_id': 'authorization'},
+    {'id': 8, 'name': 'phone', 'type': 'phone_number', 'template_id': 'catalog'},
+    {'id': 9, 'name': 'date', 'type': 'date', 'template_id': 'catalog'},
+    {'id': 10, 'name': 'description', 'type': 'text', 'template_id': 'catalog'},
+    {'id': 11, 'name': 'phone', 'type': 'phone_number', 'template_id': 'order'},
+    {'id': 12, 'name': 'date', 'type': 'date', 'template_id': 'order'},
+]
